0802684b6a04adfcf6fbfbb34488cbcd2d785bf0
JJ Jitu Ahsan <ahsanjjjitu@gmail.com>
2026-07-21 17:20:53 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,81 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const {OAuth2Client} = require('google-auth-library');
+const jwt = require('jsonwebtoken');
+const User = require('./models/User');
+
+
+const app = express();
+
+// google auth client setup
+const client = new OAuth2Client(process.env.GOOFLE_CLIENT_ID);
+
+// MongoDB Conection
+mongoose.connect(process.env.MONGO_URL)
+.then(() => console.log('MongoDB Connected'))
+.catch(err => console.error('MongoDB Connection Error', err));
+
+
+// Google login/ Signup
+app.post('/api/auth/google-login', async (req, res) => {
+
+try{
+
+    const {idToken} = req.body;
+    if (!idToken) {
+            return res.status(400).json({ success: false, message: "idToken is required" });
+        }
+
+    // verify idToken
+    const ticket = await client.verifyIdToken({
+        idToken,
+        audience: process.env.GOOFLE_CLIENT_ID
+    });
+
+    const payload = ticket.getPayload();
+    const {sub: googleId, email} = payload;
+
+    // Find or create user in MongoDB
+    let user = await User.findOne({googleId});
+    if(!user){
+        user = new User({googleId, email});
+        await user.save();
+    }
+
+
+    // Generate JWT
+    const token = jwt.sign({userId: user._id}, process.env.JWT_SECRET);
+
+    res.status(200).json({
+        success: true,
+        message: "Login successfull",
+        userId: user._id,
+        token: apptoken
+    });
+
+
+
+
+
+}
+catch(error){
+console.error("Auth Error:", error);
+        res.status(401).json({
+            success: false,
+            message: "Invalid Google Token",
+            error: error.message
+        });
+}
+
+
+
+
+
+});
+
+
+const PORT = process.env.PORT // 5000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT} ⚡`);
+});
